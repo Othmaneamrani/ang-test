9bbf524f254f3c77af118ad69eaef22de1999056
Othmaneamrani <othmaneamrani31@gmail.com>
2024-06-05 02:22:54 +0100

    actions delete

--- test-ang/src/app/app-routing.module.ts
+++ test-ang/src/app/app-routing.module.ts
@@ -7,9 +7,15 @@ import {AuthComponent} from "./auth/auth.component";
 import {guardAuthGuard} from "./guard/guard-auth.guard";
 import {SignComponent} from "./sign/sign.component";
 import {nonAuthGuard} from "./guard/non-auth.guard";
+import {CreateComponent} from "./create/create.component";
+import {UpdateComponent} from "./update/update.component";
+import {DetailsComponent} from "./details/details.component";
 
 const routes: Routes = [
   {path:'home',component:HomeComponent , canActivate:[guardAuthGuard]},
+  {path:'create',component:CreateComponent , canActivate:[guardAuthGuard]},
+  {path:'update',component:UpdateComponent , canActivate:[guardAuthGuard]},
+  {path:'details',component:DetailsComponent , canActivate:[guardAuthGuard]},
   {path:'catalogue',component:CatalogueComponent , canActivate:[guardAuthGuard]},
   {path:'about',component:AboutComponent , canActivate:[guardAuthGuard]},
   {path:'',component:AuthComponent, canActivate:[nonAuthGuard] },
--- test-ang/src/app/catalogue/catalogue.component.ts
+++ test-ang/src/app/catalogue/catalogue.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-catalogue',
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css'
 })
-export class CatalogueComponent {
+export class CatalogueComponent implements OnInit{
+  constructor(private service:ProductService, public   router:Router) {
+  }
+  public products:any = [{}];
+ngOnInit() {
+  this.getAllProducts();
+}
+
+  getAllProducts():any {
+  return this.service.getAllProducts().subscribe(
+    (resp:any)=>{
+      console.log(resp);
+      this.products=resp;
+    },
+    (error:any)=>{
+      console.log(error);
+    }
+  );
+}
+
+
+handleDeleteProduct(id:bigint):any{
+  this.service.deleteProductById(id).subscribe(
+    (resp:any)=>{
+      this.products = this.products.filter((p:any) => p.id !== id);
+    },
+    (err:any)=>{
+      console.log(err);
+    }
+  );
+}
+
+
+
+
+
 
 }
