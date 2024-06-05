7205e10b48caeb95d4c6c427a25e93aa2a256d82
Othmaneamrani <othmaneamrani31@gmail.com>
2024-06-05 02:47:40 +0100

    finish

--- test-ang/src/app/create/create.component.ts
+++ test-ang/src/app/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-create',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  public product:any = {};
+  constructor(public router:Router,private service:ProductService) {
+  }
+
+  handleCreateProduct(product:any):any{
+    this.service.createProduct(product).subscribe(
+      (resp:any)=>{
+        this.router.navigate(['/catalogue']);
+      },
+      (err:any)=>{
+        console.log(err);
+      }
+    );
+  }
+
 
 }
--- test-ang/src/app/update/update.component.ts
+++ test-ang/src/app/update/update.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrl: './update.component.css'
 })
-export class UpdateComponent {
+export class UpdateComponent implements OnInit{
+
+  public product:any = {};
+  constructor(public router:Router,private service:ProductService) {
+    const navigation = this.router.getCurrentNavigation();
+    this.product = navigation?.extras?.state?.['product'];
+  }
+
+  ngOnInit() {
+    if (!this.product) {
+      this.router.navigate(['/catalogue']);
+    }
+  }
+
+
+
+  handleUpdateProduct(product:any):any{
+    this.service.createProduct(product).subscribe(
+      (resp:any)=>{
+        this.router.navigate(['/catalogue']);
+      },
+      (err:any)=>{
+        console.log(err);
+      }
+    );
+  }
+
 
 }
